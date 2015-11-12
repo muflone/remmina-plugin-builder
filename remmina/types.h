13557d9e99f7e9c2add10d6912aeede9af6d1c33
Muflone <webreg@vbsimple.net>
2015-11-13 00:40:15 +0100

    Updated types

--- remmina/types.h
+++ remmina/types.h
@@ -1,6 +1,7 @@
 /*
  * Remmina - The GTK+ Remote Desktop Client
  * Copyright (C) 2010 Vic Lee 
+ * Copyright (C) 2014-2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,8 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, 
- * Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301, USA.
  *
  *  In addition, as a special exception, the copyright holders give
  *  permission to link the code of portions of this program with the
@@ -74,7 +75,6 @@ typedef enum
     REMMINA_PROTOCOL_SETTING_TYPE_PASSWORD,
     REMMINA_PROTOCOL_SETTING_TYPE_RESOLUTION,
     REMMINA_PROTOCOL_SETTING_TYPE_KEYMAP,
-    REMMINA_PROTOCOL_SETTING_TYPE_SCALE,
 
     REMMINA_PROTOCOL_SETTING_TYPE_TEXT,
     REMMINA_PROTOCOL_SETTING_TYPE_SELECT,
